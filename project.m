3f8d4d3696baa6f6212d5304d0c80badc33f8fe8
mabobakr <m.abobakr10@yahoo.com>
2021-01-10 19:11:53 +0200

    Add: demodulation with phase shift

--- project.m
+++ project.m
@@ -1,32 +1,65 @@
 w1 = 2*pi*10000;     % omega's
 w2 = 2*pi*20000;
 
-lowPassThresh = 0.06;
+lowPassThresh = 0.001;   % filtering threshold
 
 % read signals
-[y1, fs] = audioread("sig4.wav");
+[y1, fs] = audioread("sig6.wav");
 [y2, ] = audioread("sig5.wav");
-[y3, ] = audioread("sig1.wav");
+[y3, ] = audioread("sig4.wav");
 
-totalTime = 10;  %sec
-T = 1/ fs;
-L = totalTime * fs;  %length = time  *number of samples in one second
-t = T: T : totalTime;
 
-y1 = transpose(y1(:,2));     % one channel only
+totalTime = 10;  % total time of signals
+T = 1/ fs;       % time step
+
+% total number of samples
+L = totalTime * fs;  %length = time * number of samples in one second
+
+t = T: T : totalTime;   % time vector (for x axis)
+
+% Taking one channel only
+y1 = transpose(y1(:,2));     
 y2 = transpose(y2(:,2));
 y3 = transpose(y3(:, 2));
 
 % modulated signal
 mod_sig = y1 .* cos(t*w1) + y2 .* cos(t*w2) + y3 .* sin(t*w2);
 
-% demodulate signals
+% demodulate signals with no shift
 de_y1 = mod_sig .* cos(t*w1);
 de_y2 = mod_sig .* cos(t*w2);
 de_y3 = mod_sig .* sin(t*w2);
 de_y1 = lowpass(de_y1, lowPassThresh);
 de_y2 = lowpass(de_y2, lowPassThresh);
 de_y3 = lowpass(de_y3, lowPassThresh);
+
+% The following are demodulating with different 
+% phase shifts
+% uncomment to listen to the output (one at a time)
+
+% demodulate signals with 10 shift
+% de_y1 = mod_sig .* cos(t*w1 + 10);
+% de_y2 = mod_sig .* cos(t*w2 + 10);
+% de_y3 = mod_sig .* sin(t*w2 + 10);
+% de_y1 = lowpass(de_y1, lowPassThresh);
+% de_y2 = lowpass(de_y2, lowPassThresh);
+% de_y3 = lowpass(de_y3, lowPassThresh);
+
+% demodulate signals with 30 shift
+% de_y1 = mod_sig .* cos(t*w1 + 30);
+% de_y2 = mod_sig .* cos(t*w2 + 30);
+% de_y3 = mod_sig .* sin(t*w2 + 30);
+% de_y1 = lowpass(de_y1, lowPassThresh);
+% de_y2 = lowpass(de_y2, lowPassThresh);
+% de_y3 = lowpass(de_y3, lowPassThresh);
+
+% demodulate signals with 90 shift
+% de_y1 = mod_sig .* cos(t*w1 + 90);
+% de_y2 = mod_sig .* cos(t*w2 + 90);
+% de_y3 = mod_sig .* sin(t*w2 + 90);
+% de_y1 = lowpass(de_y1, lowPassThresh);
+% de_y2 = lowpass(de_y2, lowPassThresh);
+% de_y3 = lowpass(de_y3, lowPassThresh);
 
 
 % plot modulated signal
@@ -39,21 +72,6 @@ figure
 plot(f, mag);
 
 
-% plot demodulated signal 1 spectrum
-figure
-[mag, f] = frequency(de_y1, L, fs);
-plot(f, mag);
-
-% plot demodulated signal 2 spectrum
-figure
-[mag, f] = frequency(de_y2, L, fs);
-plot(f, mag);
-
-% plot demodulated signal 3 spectrum
-figure
-[mag, f] = frequency(de_y3, L, fs);
-plot(f, mag);
-
-% play demodulated signal
-sound(2*de_y3, fs);
-
+sound(2*de_y1, fs);
+% sound(2*de_y2, fs);
+% sound(2*de_y3, fs);
--- testing.m
+++ testing.m
@@ -1,7 +1,7 @@
-[y, fs] = audioread("Lil Wayne - 6 Foot 7 Foot (feat. Cory Gunz).wma");
+[y, fs] = audioread("Eminem - The Monster.flac");
 
-samples = [1, 10*fs]
+samples = [1, 10*fs];
 
-[y, fs] = audioread("Lil Wayne - 6 Foot 7 Foot (feat. Cory Gunz).wma", samples);
-audiowrite("sig5.wav", y, fs);
+[y, fs] = audioread("Eminem - The Monster.flac", samples);
+audiowrite("sig6.wav", y, fs);
 sound(y, fs);
