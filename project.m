ea8020a327e6bc89c838a380265980aec8eabaa0
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-01-12 18:35:17 +0200

    Add phse shift

--- project.m
+++ project.m
@@ -21,13 +21,38 @@ y3 = transpose(y3(:, 2));
 mod_sig = y1 .* cos(t*w1) + y2 .* cos(t*w2) + y3 .* sin(t*w2);
 
 % demodulate signals
-de_y1 = mod_sig .* cos(t*w1);
-de_y2 = mod_sig .* cos(t*w2);
-de_y3 = mod_sig .* sin(t*w2);
-de_y1 = lowpass(de_y1, lowPassThresh);
-de_y2 = lowpass(de_y2, lowPassThresh);
-de_y3 = lowpass(de_y3, lowPassThresh);
+de_y1 = demodulate(lowPassThresh, mod_sig, cos(t*w1))
+de_y2 = demodulate(lowPassThresh, mod_sig, cos(t*w2))
+de_y3 = demodulate(lowPassThresh, mod_sig, sin(t*w2))
 
+% sound(2*de_y1, fs);
+% sound(2*de_y2, fs);
+% sound(2*de_y3, fs);
+
+% Phse Shift
+de_y1_10 = demodulate(lowPassThresh, mod_sig, cos(t*w1+10))
+de_y2_10 = demodulate(lowPassThresh, mod_sig, cos(t*w2+10))
+de_y3_10 = demodulate(lowPassThresh, mod_sig, sin(t*w2+10))
+
+sound(2*de_y1_10, fs);
+sound(2*de_y2_10, fs);
+sound(2*de_y3_10, fs);
+
+de_y1_30 = demodulate(lowPassThresh, mod_sig, cos(t*w1+30))
+de_y2_30 = demodulate(lowPassThresh, mod_sig, cos(t*w2+30))
+de_y3_30 = demodulate(lowPassThresh, mod_sig, sin(t*w2+30))
+
+sound(2*de_y1_30, fs);
+sound(2*de_y2_30, fs);
+sound(2*de_y3_30, fs);
+
+de_y1_90 = demodulate(lowPassThresh, mod_sig, cos(t*w1+90))
+de_y2_90 = demodulate(lowPassThresh, mod_sig, cos(t*w2+90))
+de_y3_90 = demodulate(lowPassThresh, mod_sig, sin(t*w2+90))
+
+sound(2*de_y1_90, fs);
+sound(2*de_y2_90, fs);
+sound(2*de_y3_90, fs);
 
 % plot modulated signal
 figure 
@@ -55,5 +80,5 @@ figure
 plot(f, mag);
 
 % play demodulated signal
-sound(2*de_y3, fs);
+% sound(2*de_y1, fs);
 
